5e49ca3957a5aaedf3d0f56368bb9d2db6ed65ba
tusharp1171 <ertusharp1998@gmail.com>
2024-09-10 12:50:10 +0530

    Eureka Server Code push

---
+++ Eureka/service4/src/main/java/com/example/service4/Service4Application.java
@@ -0,0 +1,13 @@
+package com.example.service4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service4Application.class, args);
+	}
+
+}
